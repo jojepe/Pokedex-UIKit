c155af3b0abfd76adb6bb64b966cff60016d46fc
jojepe <joaop.mendes@icloud.com>
2025-07-26 23:24:45 -0300

    Projeto Criado

---
+++ Pokedex UIKit/Pokedex UIKit/Pokedex_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_UIKitApp.swift
+//  Pokedex UIKit
+//
+//  Created by Joje on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
